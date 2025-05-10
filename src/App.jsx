8d43600694cb65b13056845152a284e8a8f4e626
Vecthor <196379324+vecthoris@users.noreply.github.com>
2025-05-11 01:57:50 +0200

    test change

--- src/App.jsx
+++ src/App.jsx
@@ -15,11 +15,11 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
+      </div> {/* test */}
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          count is {count} 
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
