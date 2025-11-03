eb4716c3038b88cadd582fec472d585ea561e499
Vecthor <196379324+vecthoris@users.noreply.github.com>
2025-11-03 22:38:13 +0100

    feat(backend): Enable different server proxy target

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,4 +11,13 @@ export default defineConfig({
       },
     }),
   ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:5000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
